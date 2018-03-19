42e0a779bf17ca8ca235ab71db5bc341d7f88ad5
David Fulop <david.fulop@7digital.com>
2018-03-19 15:32:40 +0000

    Register service. (Doh!)

--- src/Oggstreamer/Startup.cs
+++ src/Oggstreamer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Oggstreamer.Providers;
 
 namespace Oggstreamer
 {
@@ -23,6 +24,7 @@ namespace Oggstreamer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMediaStreamProvider, MediaStreamProvider>();
             services.AddMvc();
         }
 
