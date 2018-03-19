aa79eb813bde3cea0dbbd58863ff0091c2f02f83
David Fulop <david.fulop@7digital.com>
2018-03-19 11:49:03 +0000

    Add streamer project and unit test project.

---
+++ src/Oggstreamer/Controllers/StreamController.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Oggstreamer.Controllers
+{
+    [Route("api/[controller]")]
+    public class StreamController : Controller
+    {
+        [HttpGet]
+        public FileStreamResult GetStream()
+        {
+            var stream = new MemoryStream(Encoding.ASCII.GetBytes("Hello World"));
+            return new FileStreamResult(stream, new MediaTypeHeaderValue("text/plain"))
+            {
+                FileDownloadName = "test.txt"
+            };
+        }
+    }
+}
---
+++ tests/Oggstreamer.UnitTests/StreamControllerTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Oggstreamer.UnitTests
+{
+    public class StreamControllerTests
+    {
+        [Test]
+        public void GetStream_Returns_200OK()
+        {
+            Assert.Pass("yo");
+        }
+    }
+}
